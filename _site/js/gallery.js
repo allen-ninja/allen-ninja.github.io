b96594971f5e81ca752a7b1e0795423c78c5e65d
Al <allen_@fastmail.com>
2018-11-06 18:02:59 -0700

    Add preloading images to gallery

--- _site/js/gallery.js
+++ _site/js/gallery.js
@@ -6,25 +6,27 @@ function activateGallery() {
   let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
   let mainImage  = document.querySelector("#gallery-photo img");
 
-
   thumbnails.forEach(function(thumbnail) {
-    thumbnail.addEventListener("click", function() {
-      // Set clicked image as display image.
-      let newImageSrc = thumbnail.dataset.largeVersion;
-      mainImage.setAttribute("src", newImageSrc);
+    // Preload large images.
+    let newImageSrc = thumbnail.dataset.largeVersion;
+    let largeVersion = new Image();
+    largeVersion.src = newImageSrc;
 
-      // Set alt tag of the display image.
-      let newImageAlt = thumbnail.dataset.title;
-      mainImage.setAttribute("alt", newImageAlt);
+    thumbnail.addEventListener("click", function() {
+      // Set clicked image as main image.
+      mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", thumbnail);
 
       // Change which image is current.
-      document.querySelector(".current").classList.remove("current");
-      thumbnail.parentNode.classList.add("current");
+      const className = "current";
+      document.querySelector(`.${className}`).classList.remove(className);
+      thumbnail.parentNode.classList.add(className);
 
-      // Update image info
+      // Update image info.
       let galleryInfo = document.querySelector("#gallery-info");
       let title       = galleryInfo.querySelector(".title");
       let description = galleryInfo.querySelector(".description");
+
       title.innerHTML       = thumbnail.dataset.title;
       description.innerHTML = thumbnail.dataset.description;
     });
